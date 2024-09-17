8112959c9642089f7db07820211447507099c997
x9laaa <acarreno95@outlook.com>
2024-09-17 01:22:06 -0300

    actividad 9 terminada

--- app/src/main/java/cl/bootcamp/ind_6v2/MainActivity.java
+++ app/src/main/java/cl/bootcamp/ind_6v2/MainActivity.java
@@ -2,43 +2,37 @@ package cl.bootcamp.ind_6v2;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioGroup;
-
 import androidx.appcompat.app.AppCompatActivity;
+import cl.bootcamp.ind_6v2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnElegir, btnConfirmar, btnCancelar;
-    RadioGroup rdGroup;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnElegir = findViewById(R.id.btnElegir);
-        btnConfirmar = findViewById(R.id.btnConfirmar);
-        btnCancelar = findViewById(R.id.btnCancelar);
-        rdGroup = findViewById(R.id.rdGroup);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         fondodefault();
 
-        btnElegir.setOnClickListener(v -> {
-            rdGroup.setVisibility(View.GONE);
-            btnElegir.setVisibility(View.GONE);
-
-            btnConfirmar.setVisibility(View.VISIBLE);
-            btnCancelar.setVisibility(View.VISIBLE);
+        binding.btnElegir.setOnClickListener(v -> {
+            binding.rdGroup.setVisibility(View.GONE);
+            binding.btnElegir.setVisibility(View.GONE);
+            binding.btnConfirmar.setVisibility(View.VISIBLE);
+            binding.btnCancelar.setVisibility(View.VISIBLE);
             cambiofondo();
         });
 
-        btnCancelar.setOnClickListener(v -> {
-            rdGroup.setVisibility(View.VISIBLE);
-            btnElegir.setVisibility(View.VISIBLE);
+        binding.btnCancelar.setOnClickListener(v -> {
+            binding.rdGroup.setVisibility(View.VISIBLE);
+            binding.btnElegir.setVisibility(View.VISIBLE);
 
-            btnConfirmar.setVisibility(View.GONE);
-            btnCancelar.setVisibility(View.GONE);
+            binding.btnConfirmar.setVisibility(View.GONE);
+            binding.btnCancelar.setVisibility(View.GONE);
             fondodefault();
         });
     }
@@ -48,16 +42,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void cambiofondo(){
-        int selectedId = rdGroup.getCheckedRadioButtonId();
+        int selectedId = binding.rdGroup.getCheckedRadioButtonId();
         if (selectedId == R.id.rbpoke1) {
             getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer, new CharmanderFragment()).commit();
-            btnConfirmar.setBackgroundColor(getColor(R.color.colorAccent));
+            binding.btnConfirmar.setBackgroundColor(getColor(R.color.colorAccent));
         } else if (selectedId == R.id.rbpoke2) {
             getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer, new BulbasaurFragment()).commit();
-            btnConfirmar.setBackgroundColor(getColor(R.color.verde));
+            binding.btnConfirmar.setBackgroundColor(getColor(R.color.verde));
         } else if (selectedId == R.id.rbpoke3) {
             getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer, new SquirtleFragment()).commit();
-            btnConfirmar.setBackgroundColor(getColor(R.color.azul));
+            binding.btnConfirmar.setBackgroundColor(getColor(R.color.azul));
 
         }
     }
